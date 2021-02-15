c73cb4ff2f04c8655c56804aab0ad6aac9bf169a
Sawi <skalakul@gmail.com>
2021-02-15 21:40:43 +0100

    README

--- script.js
+++ script.js
@@ -1,89 +1,109 @@
-//Global Decaration
-//What we use in the caculation
-let numbers = [0,0];
+/*Global Variables */
+
+const digits = [...Array(10).keys()];  //=> [0,1,2...,9]
+const operators = ['+','-','*','/'];
+const decimal = '.';
+const reset = 'AC';
+let enter = 'Enter';
+let numbers = ['0','0'];
 let operator = '';
-//What we use to check conditions
-let decimalCount,enterCount;
-//Dynamic variable 
-let currentNumber, currentIndex;
+let enterCount = 0;
+let currentNumber = '0'; 
+let currentIndex = 0;
 
-let monitorShow = document.getElementById('monitor');
+let display = document.getElementById('monitor');
 
-init();
+/* Initialize the Calculation */
 
-const init = () =>{
-    monitorShow.textContent = '0';
-    numbers = [0,0];
+const init = () => {
+    display.textContent = '0';
+    arrNumbers = [0,0];
     operator = '';
-    decimalCount = 0;
     enterCount = 0;
     currentNumber = '0';
     currentIndex = 0;
-}
+};
 
-const setNumbers = () => numbers[currentIndex] = monitorShow.textContent;
+init();
 
-const storeDecimal = value =>{
-    if(decimalCount== 0 && enterCount == 0){
-        decimalCount = 1;
-        currentNumber += value.toString();
+/* Update the Display */
+
+const updateDisplay = (value) => display.textContent = value;
+
+/* Set Numbers, Operator and Decimal */
+
+const setArrNumbers = () => numbers[currentIndex] = display.textContent;
+
+const setCurrentNumber = digit =>{
+    
+    if(currentNumber !== '0' && enterCount === 0){
+        currentNumber += digit.toString();
     }
-    else if (enterCount != 0){
-        decimalCount = 1;
-        currentNumber = '0' + value.toString();
+    else if(currentNumber !== '0' && enterCount !== 0){
+        currentNumber += digit.toString();
+        enterCount = 0;
     }
-    monitorShow.textContent= currentNumber;
-}
-
-const setCurrentNumber = value =>{
-
-    if(currentNumber != '0' && enterCount== 0){
-        currentNumber += value.toString();
-    }
-    else if(currentNumber != '0' && enterCount != 0){
-        currentNumber += value.toString();
-        enterCount= 0;
+    else if (currentNumber === '0' && digit === 0){
+        currentNumber = '0';
     }
     else{
-        currentNumber=value;
+        currentNumber = digit;
     }
-    setNumbers();
-    monitorShow.textContent= currentNumber;
+
+    updateDisplay(currentNumber);
+    setArrNumbers();
+
 }
 
 const setOperator = opt =>{
+
     operator = opt;
-    setNumbers();
-    monitorShow.textContent= opt;
+    setArrNumbers();
+    updateDisplay(opt);
     currentIndex = 1;
-    currentNumber='0';
-    decimalCount = 0;
+    currentNumber = '0';
+
+}
+
+const storeDecimal = value => {
+
+    if (!currentNumber.toString().indexOf(value) > -1 && enterCount == 0 ){
+        currentNumber += value;
+    }
+    else if (enterCount !== 0){
+        currentNumber = '0.';
+    }
+    updateDisplay(currentNumber);
 }
 
 const calculation = () => {
 
     if(operator != ''){
-        setNumbers();
-        let total=0;
+        setArrNumbers();
+        let total = 0;
+        let num0 = numbers[0].includes('.') ? parseFloat(numbers[0]) : parseInt(numbers[0]);
+        let num1 = numbers[1].includes('.') ? parseFloat(numbers[1]) : parseInt(numbers[1]);
+
         switch(operator){
-            case "+": total = parseFloat(numbers[0]) + parseFloat(numbers[1]);break;
-            case "-": total = numbers[0] - numbers[1];break;
-            case "*": total = numbers[0] * numbers[1];break;
-            case "%": if(numbers[1]!= 0){total = numbers[0] / numbers[1];}
-                      else{window.alert()("The number can not be devided by 0");} break;
+            case "+": total = num0 + num1;break;
+            case "-": total = num0 - num1;break;
+            case "*": total = num0 * num1;break;
+            case "%": if(num1!== 0){total = num0 / num1;}
+                      else{window.alert("The number can not be devided by 0");} break;
         }
-        monitorShow.textContent= total;
+        updateDisplay(total);
         //Set values for the next caculation
-        currentIndex =0;
-        setNumbers();
+        currentIndex = 0;
+        setArrNumbers();
         operator = '';
-        decimalCount = 0;
         enterCount = 1;
+        currentNumber = '0';
 
     }
 }
 
-//When a user clicks on the button, store the value
+/* Handle key presses */
+
 document.querySelector('.btn--ac').addEventListener('click',init);
 document.querySelector('.btn--enter').addEventListener('click',calculation);
 
